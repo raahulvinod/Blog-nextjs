cac786b68240bf8d52c0fe3e4d475a26c09df7c8
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-02 23:29:49 +0530

    Hero Section Added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>©2023 Dev Sphere. All rights reserved</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          alt="facebook logo"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/2.png"
+          alt="instagram logo"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/3.png"
+          alt="twitter logo"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/4.png"
+          alt="youtube logo"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+      </div>
     </div>
   );
 };
