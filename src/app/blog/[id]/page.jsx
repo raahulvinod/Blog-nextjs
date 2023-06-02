0c2e443ca870f476b9fc5cc7dda62417cf8b5578
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-03 02:28:08 +0530

    Blog Page And Detail Page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 const BlogPost = () => {
-  return <div>Blogpost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>desc</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/123335/pexels-photo-123335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>content</p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
