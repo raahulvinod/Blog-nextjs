5ba9f441f1f89dbe0cd2e31a2c380c6e3177a772
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-04 16:27:27 +0530

    Data Fetching

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import styles from './page.module.css';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: { revalidate: 10 },
+  });
+
+  if (!res.ok) {
+    throw notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>title</h1>
-          <p className={styles.desc}>desc</p>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/123335/pexels-photo-123335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.pexels.com/photos/4449317/pexels-photo-4449317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>username</span>
+            <span className={styles.username}>Rahul vinod</span>
           </div>
         </div>
+
         <div className={styles.imageContainer}>
           <Image
             src="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -30,7 +50,7 @@ const BlogPost = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>content</p>
+        <p className={styles.text}>{data.body}</p>
       </div>
     </div>
   );
