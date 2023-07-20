eb6ed463b4962f41fe62d77a4d275f155c376ea2
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-20 20:46:00 +0530

    User Model and Post Model Added

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const { schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+
+      required: true,
+    },
+    desc: {
+      type: String,
+
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);
